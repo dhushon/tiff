55015b8ca032a9491d5275de4d2db9d64d00d29a
Dan Hushon <dhushon@guidehouse.com>
2025-06-09 15:15:05 -0400

    updated tiff from unmaintained ca 2014 lib

--- apps/tiffinfo.go
+++ apps/tiffinfo.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//go:build ingore
 // +build ingore
 
 package main
@@ -11,7 +12,7 @@ import (
 	"log"
 	"os"
 
-	tiff "github.com/chai2010/tiff"
+	tiff "github.com/dhushon/tiff"
 )
 
 func main() {
